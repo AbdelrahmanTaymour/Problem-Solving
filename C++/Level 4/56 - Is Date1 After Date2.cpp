97c7733e05d4c8d66a47f58ab2b564920cb147ed
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-06-14 18:59:08 +0300

    Create 56 - Is Date1 After Date2.cpp

---
+++ C++/Level 4/56 - Is Date1 After Date2.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+//Problem 56 - Is Date1 After Date2
+
+struct stDate
+{
+    short Year;
+    short Month;
+    short Day;
+};
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+short ReadMonth()
+{
+    short Month;
+    cout << "Please enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+short ReadYear()
+{
+    short Year;
+    cout << "Please enter a Year? ";
+    cin >> Year;
+    return Year;
+}
+stDate ReadFullDate()
+{
+    stDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year < Date2.Year) ? true : ((Date1.Year ==
+        Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month ==
+            Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+bool IsDate1EqualDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((Date1.Month ==
+        Date2.Month) ? ((Date1.Day == Date2.Day) ? true : false) : false)
+        : false;
+}
+bool IsDate1AfterDate2(stDate Date1, stDate Date2)
+{
+    return (!IsDate1BeforeDate2(Date1, Date2) && !IsDate1EqualDate2(Date1, Date2));
+}
+
+int main()
+{
+    //Problem 56 - Is Date1 After Date2
+
+    cout << "\nEnter Date1:";
+    stDate Date1 = ReadFullDate();
+    cout << "\nEnter Date2:";
+    stDate Date2 = ReadFullDate();
+
+    if (IsDate1AfterDate2(Date1, Date2))
+        cout << "\nYes, Date1 is After Date2.";
+    else
+        cout << "\nNo, Date1 is NOT After Date2.";
+}
