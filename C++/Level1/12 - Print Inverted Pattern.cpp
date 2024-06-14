d6f8b0125a9f89806c0eae9fce851d42523c014c
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-06-14 11:57:00 +0300

    Create 12 - Print Inverted Pattern.cpp

---
+++ C++/Level1/12 - Print Inverted Pattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//Problem 12 - Print Inverted Pattern
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+void PrintInvertedPattern(int Number)
+{
+    for (int i = Number;i >= 1;i--)
+    {
+        for (int j = 1;j <= i;j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
