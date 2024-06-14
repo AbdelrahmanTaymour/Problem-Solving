63a47e2225d5cc0c4a0b8bae1423a46910b57b98
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-06-14 14:22:15 +0300

    Create 4 - Sum Each Col in Matrix.cpp

---
+++ C++/Level 3/4 - Sum Each Col in Matrix.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+//Problem 4 - Sum Each Col in Matrix
+
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+void FillRandomMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0;i < Rows;i++)
+    {
+        for (int j = 0;j < Cols;j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+int sumCol(int arr[3][3], short ColNumber, short Rows)
+{
+    int sum = 0;
+    for (short j = 0;j < Rows;j++)
+    {
+        sum += arr[j][ColNumber];
+    }
+    return sum;
+}
+
+void SumColsInMatrix(int arr[3][3], short Rows, short Cols)
+{
+    cout << "\nThe following are the sum of each col in the matrix:\n";
+    for (int i = 0;i < Cols;i++)
+    {
+        cout << " Col " << i + 1 << " Sum = " << sumCol(arr, i, Rows) << endl;
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    cout << "\nThe folowing is a 3x3 Matrix:\n";
+    for (int i = 0;i < Rows;i++)
+    {
+        for (int j = 0;j < Cols;j++)
+        {
+            cout << setw(3) << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    //Problem 4 - Sum Each Col in Matrix
+
+    srand((unsigned)time(NULL));
+    int arr[3][3];
+    FillRandomMatrix(arr, 3, 3);
+    PrintMatrix(arr, 3, 3);
+    SumColsInMatrix(arr, 3, 3);
+}
