70bf2a4cfb209dd606586641948c29e41de901b5
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-06-14 11:11:57 +0300

    Create 46- Print Letters from A to Z.cpp

---
+++ C++/Level1/46- Print Letters from A to Z.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Problem 46 - Print Letters from A to Z
+
+void PrintLettersAtoZ()
+{
+	for (int i = 65;i <= 90;i++) 
+	{
+		cout << char(i) << endl;
+	}
+}
+
+int main()
+{
+	//Problem 46 - Print Letters from A to Z
+	PrintLettersAtoZ();
+}
