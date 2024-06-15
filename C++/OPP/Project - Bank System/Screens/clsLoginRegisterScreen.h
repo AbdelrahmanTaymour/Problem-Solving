cd78123a483e880e91e0befdea52cf6b15b6b4a3
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-06-15 15:45:58 +0300

    Create clsLoginRegisterScreen.h

---
+++ C++/OPP/Project - Bank System/Screens/clsLoginRegisterScreen.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include "clsInputValidate.h"
+#include "clsAddNewUserScreen.h"
+#include "clsLoginScreen.h"
+#include "clsRegister.h"
+
+class clsLoginRegisterScreen
+{
+public:
+	static bool ShowLoginRegisterScreen()
+	{
+		system("cls");
+
+		cout << "\t\t\t\t\t______________________________________";
+		cout << "\n\n\t\t\t\t\t  " << "[1] Login Screen";
+		cout << "\n\n\t\t\t\t\t  " << "[2] Register Screen";
+		cout << "\n\t\t\t\t\t______________________________________\n\n";
+
+		cout << "\n\nEnter your Choice : ";
+		int Choice = clsInputValidate::ReadShortNumberBetween(1, 2,"Invalid Choice. Plase Enter [1] to Login or [2] for Register: ");
+
+		if (Choice == 1)
+			return clsLoginScreen::ShowLoginScreen();
+		else
+			return clsRegisterScreen::ShowRegisterScreen();
+	}
+};
