00df1ad4737b4d7291790d07d6a9e571fbf70062
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-06-15 15:49:38 +0300

    Create clsRegister.h

---
+++ C++/OPP/Project - Bank System/Screens/clsRegister.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include "clsScreen.h"
+#include "clsInputValidate.h"
+#include "clsAddNewUserScreen.h"
+#include "clsMainScreen.h"
+#include "clsLoginScreen.h"
+
+class clsRegisterScreen : protected clsScreen
+{
+private:
+	static bool _Register()
+	{
+		system("cls");
+
+		clsAddNewUserScreen::ShowAddNewUserScreen();
+		CurrentUser.RegisterLogIn();
+		enMode::Login;
+
+		cout << "\n\nDone. Please Press any key to go to Main Screen...\n\n";
+		system("pause>0");
+		clsMainScreen::ShowMainMenue();
+		return true;
+	}
+
+
+public:
+	static bool ShowRegisterScreen()
+	{
+		system("cls");
+		_DrawScreenHeader("\t  [1] Register Screen");
+		return _Register();
+	}
+};
