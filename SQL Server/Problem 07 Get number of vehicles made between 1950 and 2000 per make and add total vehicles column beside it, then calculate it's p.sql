d97e68d13531bfb3a849e01887d97f3b139f4973
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-09-06 21:26:53 +0300

    Update Problem 07 Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's p.sql

--- SQL Server/Problem 07 Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's p.sql
+++ SQL Server/Problem 07 Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's p.sql
@@ -1,15 +1,12 @@
 
 --Problem 7 Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's percentage
 
-
-
 Select *, CAST(NumberOfVehicle AS float) / CAST(TotalVehicle AS float) AS Perc  from
 (
-	SELECT		Makes.Make, COUNT(*) AS NumberOfVehicle, (select Count(*) from VehicleDetails As TotalVehicle) As TotalVehicle 
-	FROM		  Makes inner join 
-						VehicleDetails ON Makes.MakeID = VehicleDetails.MakeID
-	
-	WHERE		(VehicleDetails.Year BETWEEN 1950 AND 2000)
+	SELECT	Makes.Make, COUNT(*) AS NumberOfVehicle, (select Count(*) from VehicleDetails As TotalVehicle) As TotalVehicle 
+	FROM	Makes INNER JOIN 
+			VehicleDetails ON Makes.MakeID = VehicleDetails.MakeID
+	WHERE	(VehicleDetails.Year BETWEEN 1950 AND 2000)
 	GROUP BY Makes.Make
 ) R1
 ORDER BY NumberOfVehicle DESC
