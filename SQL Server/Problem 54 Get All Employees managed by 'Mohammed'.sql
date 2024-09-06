b54f1ec46d4003da35b588d7e12a37fca5c3908e
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-09-06 21:21:44 +0300

    Update Problem 54 Get All Employees managed by 'Mohammed'.sql

--- SQL Server/Problem 54 Get All Employees managed by 'Mohammed'.sql
+++ SQL Server/Problem 54 Get All Employees managed by 'Mohammed'.sql
@@ -1,9 +1,7 @@
 
 -- Problem 54 Get All Employees managed by 'Mohammed'
 
-SELECT        Employees.Name, Employees.ManagerID, Employees.Salary, Managers.Name AS ManagerName
-FROM            Employees INNER JOIN
+SELECT Employees.Name, Employees.ManagerID, Employees.Salary, Managers.Name AS ManagerName
+FROM   Employees INNER JOIN
                          Employees AS Managers ON Employees.ManagerID = Managers.EmployeeID
-where Managers.Name='Mohammed'
-
-
+WHERE Managers.Name='Mohammed';
