2472cac085d8d9d086339b9ebf36aa706b320897
Abelrahman Taymour <104181611+abdelrahmantaymour@users.noreply.github.com>
2024-09-06 21:27:50 +0300

    Update Problem 08 Get Make, FuelTypeName and Number of Vehicles per FuelType per Make.sql

--- SQL Server/Problem 08 Get Make, FuelTypeName and Number of Vehicles per FuelType per Make.sql
+++ SQL Server/Problem 08 Get Make, FuelTypeName and Number of Vehicles per FuelType per Make.sql
@@ -1,11 +1,11 @@
 
 -- Problem 8 Get Make, FuelTypeName and Number of Vehicles per FuelType per Make
 
-
-Select		Makes.Make, FuelTypes.FuelTypeName, COUNT(*) AS NumberOfVehicles
-FROM		VehicleDetails inner join 
-				Makes ON VehicleDetails.MakeID = Makes.MakeID INNER JOIN
+Select	Makes.Make, FuelTypes.FuelTypeName, COUNT(*) AS NumberOfVehicles
+FROM	VehicleDetails INNER JOIN
+			Makes ON VehicleDetails.MakeID = Makes.MakeID INNER JOIN
 				FuelTypes ON VehicleDetails.FuelTypeID = FuelTypes.FuelTypeID
-WHERE		(VehicleDetails.Year BETWEEN 1950 AND 2000)
+	
+WHERE	(VehicleDetails.Year BETWEEN 1950 AND 2000)
 GROUP BY Makes.Make, FuelTypes.FuelTypeName
 ORDER BY Makes.Make;
